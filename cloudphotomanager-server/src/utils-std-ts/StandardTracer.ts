c43b2275c56f39f99cd75872a4c580e8d6e09fe7
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2023-03-26 06:37:24 +0800

    WIP

--- cloudphotomanager-server/src/utils-std-ts/StandardTracer.ts
+++ cloudphotomanager-server/src/utils-std-ts/StandardTracer.ts
@@ -1,90 +1,58 @@
-import { BatchSpanProcessor, Span } from "@opentelemetry/sdk-trace-base";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { Resource } from "@opentelemetry/resources";
-import { AWSXRayIdGenerator } from "@opentelemetry/id-generator-aws-xray";
-
-import { SemanticAttributes, SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import opentelemetry, { Context } from "@opentelemetry/api";
-import * as os from "os";
-import { ConfigInterface } from "./models/ConfigInterface";
-import { defaultTextMapSetter, trace, ROOT_CONTEXT } from "@opentelemetry/api";
+import { FastifyInstance } from "fastify";
+import { SemanticAttributes } from "@opentelemetry/semantic-conventions";
+import { SpanStatusCode } from "@opentelemetry/api";
+import { Config } from "./Config";
+import { StandardTracer } from "./utils-std-ts/StandardTracer";
+import { Span } from "@opentelemetry/sdk-trace-base";
+import { defaultTextMapGetter, ROOT_CONTEXT } from "@opentelemetry/api";
 import { W3CTraceContextPropagator } from "@opentelemetry/core";
-import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
+import { api } from "@opentelemetry/sdk-node";
+import { Logger } from "./utils-std-ts/Logger";
 
-let tracerInstance;
-let config: ConfigInterface;
 const propagator = new W3CTraceContextPropagator();
-
-export class StandardTracer {
+const logger = new Logger("StandardTracerApi");
+export class StandardTracerApi {
   //
-  public static initTelemetry(initConfig: ConfigInterface) {
-    config = initConfig;
-    const provider = new NodeTracerProvider({
-      idGenerator: new AWSXRayIdGenerator(),
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: `${config.SERVICE_ID}`,
-        [SemanticResourceAttributes.SERVICE_VERSION]: `${config.VERSION}`,
-        [SemanticResourceAttributes.SERVICE_NAMESPACE]: "cloudphotomanager",
-        [SemanticResourceAttributes.HOST_NAME]: os.hostname(),
-      }),
-    });
-    provider.register();
-    if (config.OPENTELEMETRY_COLLECTOR_HTTP) {
-      const exporter = new OTLPTraceExporter({
-        url: config.OPENTELEMETRY_COLLECTOR_HTTP,
-        headers: {},
+  public static async registerHooks(fastify: FastifyInstance, config: Config): Promise<void> {
+    fastify.addHook("onRequest", async (req) => {
+      if (req.url.indexOf("/api") !== 0) {
+        return;
+      }
+
+      let spanName = `${req.method}-${req.url}`;
+      let urlName = req.url;
+      if (config.OPENTELEMETRY_COLLECTOR_AWS) {
+        spanName = `${config.SERVICE_ID}-${config.VERSION}`;
+        urlName = `${config.SERVICE_ID}-${config.VERSION}-${req.method}-${req.url}`;
+      }
+      const callerContext = propagator.extract(ROOT_CONTEXT, req.headers, defaultTextMapGetter);
+      api.context.with(callerContext, () => {
+        const span = StandardTracer.startSpan(spanName);
+        span.setAttribute(SemanticAttributes.HTTP_METHOD, req.method);
+        span.setAttribute(SemanticAttributes.HTTP_URL, urlName);
+        /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+        (req as any).tracerSpanApi = span;
       });
-      provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-    }
-    const contextManager = new AsyncHooksContextManager();
-    contextManager.enable();
-    opentelemetry.context.setGlobalContextManager(contextManager);
-  }
+    });
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public static getSpanFromRequest(req: any): Span {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (req as any).tracerSpanApi as Span;
-  }
+    fastify.addHook("onResponse", async (req, reply, payload) => {
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      const span = (req as any).tracerSpanApi as Span;
+      if (reply.statusCode > 299) {
+        span.status.code = SpanStatusCode.ERROR;
+      } else {
+        span.status.code = SpanStatusCode.OK;
+      }
+      span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, reply.statusCode);
+      span.end();
+    });
 
-  public static startSpan(name, parentSpan?: Span): Span {
-    const tracer = StandardTracer.getTracer();
-    let spanName = name;
-    if (config.OPENTELEMETRY_COLLECTOR_AWS) {
-      spanName = `${config.SERVICE_ID}-${config.VERSION}`;
-    }
-
-    if (parentSpan) {
-      return tracer.startSpan(
-        spanName,
-        undefined,
-        opentelemetry.trace.setSpan(opentelemetry.context.active(), parentSpan)
-      ) as Span;
-    }
-
-    const span = tracer.startSpan(spanName) as Span;
-    if (config.OPENTELEMETRY_COLLECTOR_AWS) {
-      span.setAttribute(SemanticAttributes.HTTP_URL, `${config.SERVICE_ID}-${config.VERSION}-${name}`);
-    }
-
-    return span;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public static getTracer(): any {
-    if (!tracerInstance) {
-      tracerInstance = opentelemetry.trace.getTracer(`${config.SERVICE_ID}-${config.VERSION}`);
-    }
-    return tracerInstance;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public static appendHeader(context: Span, headers = {}): any {
-    if (!headers) {
-      headers = {};
-    }
-    propagator.inject(trace.setSpanContext(ROOT_CONTEXT, context.spanContext()), headers as any, defaultTextMapSetter);
-    return headers;
+    fastify.addHook("onError", async (req, reply, error) => {
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      const span = (req as any).tracerSpanApi as Span;
+      span.status.code = SpanStatusCode.ERROR;
+      span.recordException(error);
+      logger.error(error);
+    });
   }
 }
