131c07e3a420e8182a64fdaf8d004c79e31518e9
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-02-12 20:13:31 +0800

    WIP

--- docs/dev/docker-publish-images.sh
+++ docs/dev/docker-publish-images.sh
@@ -5,21 +5,19 @@ set -e
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 cd ${REPO_DIR}
 
-function tagAndPush {
-    IMAGE_BASE=${1}
-    IMAGE_TARGET=${2}
-    docker tag ${IMAGE_BASE} ${IMAGE_TARGET}
-    docker push ${IMAGE_TARGET}
-}
-
 SERVICE_NAME="cloudphotomanager"
 SERVICE_VERSION=$(cat ${REPO_DIR}/package.json | jq -r '.version')
 SERVICE_VERSION_MAJOR=$(cat ${REPO_DIR}/package.json | grep \"version\" | cut -f4 -d"\"" | cut -f1 -d".")
 SERVICE_VERSION_MINOR=$(cat ${REPO_DIR}/package.json | grep \"version\" | cut -f4 -d"\"" | cut -f1-2 -d".")
 
-echo "Publishing ${SERVICE_NAME}/${SERVICE_VERSION}/${SERVICE_VERSION_MAJOR}/${SERVICE_VERSION_MINOR}"
+echo "Building ${SERVICE_NAME}/${SERVICE_VERSION}"
 
-tagAndPush didierhoarau/cloudphotomanager:${SERVICE_VERSION} didierhoarau/cloudphotomanager:${SERVICE_VERSION}
-tagAndPush didierhoarau/cloudphotomanager:${SERVICE_VERSION} didierhoarau/cloudphotomanager:${SERVICE_VERSION_MAJOR}
-tagAndPush didierhoarau/cloudphotomanager:${SERVICE_VERSION} didierhoarau/cloudphotomanager:${SERVICE_VERSION_MINOR}
-tagAndPush didierhoarau/cloudphotomanager:${SERVICE_VERSION} didierhoarau/cloudphotomanager:latest
+docker buildx build \
+  --platform linux/arm64/v8,linux/amd64 \
+   --push \
+  -f Dockerfile \
+  -t didierhoarau/${SERVICE_NAME}:latest \
+  -t didierhoarau/${SERVICE_NAME}:${SERVICE_VERSION} \
+  -t didierhoarau/${SERVICE_NAME}:${SERVICE_VERSION_MAJOR} \
+  -t didierhoarau/${SERVICE_NAME}:${SERVICE_VERSION_MINOR} \
+  .
