7ba6a58946b5539eed4b3262559cb143b45aa70f
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-04-10 22:33:01 +0800

    WIP

--- cloudphotomanager-web/services/FileUtils.ts
+++ cloudphotomanager-web/services/FileUtils.ts
@@ -1,7 +1,7 @@
 export class FileUtils {
   //
   public static getType(file: any) {
-    const imageExtensions = ["jpg", "jpeg", "png", "gif", "webp"];
+    const imageExtensions = ["jpg", "jpeg", "png", "gif", "webp", "heic", "dng"];
     const videoExtensions = ["mp4", "mov", "wmv", "avi", "mkv"];
     const extension = file.filename.split(".").pop().toLowerCase();
     if (imageExtensions.includes(extension)) {
